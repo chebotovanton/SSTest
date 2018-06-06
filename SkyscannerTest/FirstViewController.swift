5432f766811e723ae671ddc07c4f507da8f99a62
Anton Chebotov <anton.chebotov@aviasales.ru>
2018-06-06 15:31:17 +0300

    initial commit

---
+++ SkyscannerTest/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SkyscannerTest
+//
+//  Created by Anton Chebotov on 06/06/2018.
+//  Copyright © 2018 Anton Chebotov. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
