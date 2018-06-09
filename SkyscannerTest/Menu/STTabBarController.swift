563cb63cc2491bc070a31a2a4c6c969179a27b99
Anton Chebotov <anton.chebotov@aviasales.ru>
2018-06-09 16:52:46 +0300

    tab bar tint color

--- SkyscannerTest/Menu/STTabBarController.swift
+++ SkyscannerTest/Menu/STTabBarController.swift
@@ -5,6 +5,7 @@ class STTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        UITabBar.appearance().tintColor = UIColor(red: 0, green: 178.0/255.0, blue: 214.0/255.0, alpha: 1.0)
         if let items = tabBar.items {
             for item in items {
                 item.imageInsets = UIEdgeInsets(top: 5, left: 0, bottom: -5, right: 0)
