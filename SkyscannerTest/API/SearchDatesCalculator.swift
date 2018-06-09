e0b4b3f2d60f2e717606534680d2b64d2996f3b1
Anton Chebotov <anton.chebotov@aviasales.ru>
2018-06-09 20:45:18 +0300

    Outbound/Inbound dates calculations + tests

--- SkyscannerTest/API/SearchDatesCalculator.swift
+++ SkyscannerTest/API/SearchDatesCalculator.swift
@@ -5,28 +5,40 @@ class SearchDatesCalculator {
     private static var dateFormatter: DateFormatter {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd"
+        dateFormatter.timeZone = timeZone()
 
         return dateFormatter
     }
 
     private static var calendar: Calendar {
         var calendar = Calendar(identifier: Calendar.Identifier.gregorian)
-        calendar.timeZone = TimeZone(abbreviation: "GMT")!
+        calendar.timeZone = timeZone()
 
         return calendar
     }
 
+    private static func timeZone() -> TimeZone {
+        return TimeZone(abbreviation: "GMT")!
+    }
+
     static func nextMondayString(from date: Date) -> String {
-        let weekday = (calendar.firstWeekday == 1) ? 2 : 1
-        let nextMonday = calendar.nextDate(after: date, matching: DateComponents(weekday: weekday), matchingPolicy: .nextTime)!
+        let nextMonday = getNextMonday(from: date)
 
         return dateFormatter.string(from: nextMonday)
     }
 
     static func nextTuesdayString(from date: Date) -> String {
-        let weekday = (calendar.firstWeekday == 1) ? 3 : 2
-        let nextTuesday = calendar.nextDate(after: date, matching: DateComponents(weekday: weekday), matchingPolicy: .strict)!
+        let nextMonday = getNextMonday(from: date)
+        let nextTuesday = calendar.date(byAdding: .day, value: 1, to: nextMonday)!
 
         return dateFormatter.string(from: nextTuesday)
+    }
+
+    private static func getNextMonday(from date: Date) -> Date {
+        let targetWeekday = (calendar.firstWeekday == 1) ? 2 : 1
+        let dateWeekday = calendar.component(.weekday, from: date)
+        let daysToAdd = targetWeekday + 7 - dateWeekday
+
+        return calendar.date(byAdding: .day, value: daysToAdd, to: date)!
     }
 }
--- SkyscannerTestTests/SearchDatesCalculatorTests.swift
+++ SkyscannerTestTests/SearchDatesCalculatorTests.swift
@@ -3,36 +3,53 @@ import XCTest
 
 class SearchDatesCalculatorTests: XCTestCase {
 
-    func testSimpleWeekSwitch() {
+    var dateFormatter: DateFormatter {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd"
+
+        return dateFormatter
+    }
+
+    func testSimpleWeekSwitch() {
         let date = dateFormatter.date(from: "2018-06-06")!
         let result = SearchDatesCalculator.nextMondayString(from: date)
-        XCTAssertEqual(result, "2018-06-11", "Wrong dates calculation")
+        XCTAssertEqual(result, "2018-06-11", "SearchDatesCalculator fails next monday detection")
     }
 
     func testLastWeekOfMonth() {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd"
         let date = dateFormatter.date(from: "2018-06-29")!
         let result = SearchDatesCalculator.nextMondayString(from: date)
-        XCTAssertEqual(result, "2018-07-02", "Wrong dates calculation")
+        XCTAssertEqual(result, "2018-07-02", "SearchDatesCalculator fails next monday detection in next month")
     }
 
     func testMondayItself() {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd"
         let date = dateFormatter.date(from: "2018-06-18")!
         let result = SearchDatesCalculator.nextMondayString(from: date)
-        XCTAssertEqual(result, "2018-06-25", "Wrong dates calculation")
+        XCTAssertEqual(result, "2018-06-25", "SearchDatesCalculator fails detecting NEXT monday from some monday")
     }
 
     func testLastWeekOfYear() {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd"
         let date = dateFormatter.date(from: "2018-12-31")!
         let result = SearchDatesCalculator.nextMondayString(from: date)
-        XCTAssertEqual(result, "2019-01-07", "Wrong dates calculation")
+        XCTAssertEqual(result, "2019-01-07", "SearchDatesCalculator fails detecting next monday in next year")
+    }
+
+    func testNextTuesday() {
+        let date = dateFormatter.date(from: "2018-06-06")!
+        let result = SearchDatesCalculator.nextTuesdayString(from: date)
+        XCTAssertEqual(result, "2018-06-12", "SearchDatesCalculator fails next tuesday detection")
+    }
+
+    func testNextTuesdayFromSomeMonday() {
+        let date = dateFormatter.date(from: "2018-06-18")!
+        let result = SearchDatesCalculator.nextTuesdayString(from: date)
+        XCTAssertEqual(result, "2018-06-26", "SearchDatesCalculator fails detecting next tuesday from some monday")
+    }
+
+    func testLastWeekOfYear2() {
+        let date = dateFormatter.date(from: "2018-12-31")!
+        let result = SearchDatesCalculator.nextTuesdayString(from: date)
+        XCTAssertEqual(result, "2019-01-08", "SearchDatesCalculator fails detecting next tuesday from last monday in a year")
     }
 
 }
