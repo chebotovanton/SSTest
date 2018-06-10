52407db150835980f8ee82bd68f9e4544f48d3bb
Anton Chebotov <anton.chebotov@aviasales.ru>
2018-06-10 16:12:44 +0300

    Rating label and icon positioning

--- SkyscannerTest/Utils/PriceFormatter.swift
+++ SkyscannerTest/Utils/PriceFormatter.swift
@@ -1,8 +1,7 @@
 class PriceFormatter {
     static func priceString(itinerary: Itinerary) -> String {
         if let minPrice = itinerary.calculateMinPrice() {
-            return String(format: "£%f", minPrice)
-//            return String(format: "£%.0f", minPrice)
+            return String(format: "£%.0f", minPrice)
         }
         return "Unknown"
     }
